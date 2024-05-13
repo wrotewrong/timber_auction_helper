41ac01b433a6cbb1fa1d3da2abcf95cfbec6468e
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2024-05-13 22:04:26 +0200

    Add estimateWinner util function

--- testing.mjs
+++ testing.mjs
@@ -1,12 +1,71 @@
 import importProducts from './utils/importProducts.mjs';
 import createDoc from './utils/createDoc.mjs';
+import estimateWinner from './utils/estimateWinner.mjs';
 
 const products = [
-  { first_name: 1, last_name: 'Brzeziny', masa: 4.76, cena: 5000.62 },
-  { first_name: 2, last_name: 'Bełchatów', masa: 1.76, cena: 2783.62 },
-  { first_name: 3, last_name: 'Kutno', masa: 2.0, cena: 3333.62 },
+  {
+    productNumber: 1,
+    forestDistrict: 'Brzeziny',
+    volume: 14.76,
+    startingPrice: 5000.62,
+    maxBid: { company: undefined, offer: 0 },
+  },
+  {
+    productNumber: 2,
+    forestDistrict: 'Bełchatów',
+    volume: 11.76,
+    startingPrice: 2783.62,
+    maxBid: { company: undefined, offer: 0 },
+  },
+  {
+    productNumber: 3,
+    forestDistrict: 'Kutno',
+    volume: 12.0,
+    startingPrice: 3333.62,
+    maxBid: { company: undefined, offer: 0 },
+  },
 ];
 
-for (let product of products) {
-  createDoc('contract', product);
-}
+const companies = [
+  {
+    companyNip: '123123123',
+    companyName: 'COMPANY A',
+    minVolume: 20,
+    volumeWon: 0,
+    productsWon: [],
+  },
+  {
+    companyNip: '456456456',
+    companyName: 'COMPANY B',
+    minVolume: 10,
+    volumeWon: 0,
+    productsWon: [],
+  },
+  {
+    companyNip: '678678678',
+    companyName: 'COMPANY C',
+    minVolume: 20,
+    volumeWon: 0,
+    productsWon: [],
+  },
+];
+
+const offers = [
+  { productNumber: 1, bidder: 'COMPANY A', bid: 5600 },
+  { productNumber: 1, bidder: 'COMPANY B', bid: 5300 },
+  { productNumber: 1, bidder: 'COMPANY C', bid: 5400 },
+  { productNumber: 1, bidder: 'COMPANY D', bid: 5500 },
+  { productNumber: 2, bidder: 'COMPANY A', bid: 4500 },
+  { productNumber: 2, bidder: 'COMPANY B', bid: 4300 },
+  { productNumber: 2, bidder: 'COMPANY C', bid: 4200 },
+  { productNumber: 3, bidder: 'COMPANY A', bid: 4300 },
+  { productNumber: 3, bidder: 'COMPANY B', bid: 4500 },
+];
+
+const contracts = [];
+
+// for (let product of products) {
+//   createDoc('contract', product);
+// }
+
+estimateWinner(products, offers, companies);
---
+++ utils/estimateWinner.mjs
@@ -0,0 +1,33 @@
+const estimateWinner = (products, offers, companies) => {
+  for (let product of products) {
+    for (let offer of offers) {
+      if (
+        product.productNumber === offer.productNumber &&
+        offer.bid > product.maxBid.offer
+      ) {
+        product.maxBid.offer = offer.bid;
+        product.maxBid.company = offer.bidder;
+      }
+    }
+  }
+
+  for (let company of companies) {
+    for (let product of products) {
+      if (company.companyName === product.maxBid.company) {
+        company.volumeWon += product.volume;
+        company.productsWon.push(product.productNumber);
+      }
+    }
+  }
+
+  for (let company of companies) {
+    if (company.volumeWon < company.minVolume) {
+      company.productsWon = [];
+    }
+  }
+
+  console.log(companies);
+  console.log(products);
+};
+
+export default estimateWinner;
